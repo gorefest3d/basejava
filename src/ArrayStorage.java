1df2b6e4ef454d1d89b6d59f606abc3f444055cd
Alex Sorokin <gorefest3d@mail.ru>
2021-05-31 19:02:41 +0300

    edit methods: size(), get(), delete().

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,54 +4,52 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int size;
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i <= size(); i++) {
+        for (int i = 0; i <= size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        int storageIndex = 0;
-        for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(uuid)) storageIndex = i;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
         }
-        return storage[storageIndex];
+        return null;
     }
 
     void delete(String uuid) {
        int indexDelete = 0;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
                 indexDelete = i;
             }
         }
-        for (int i = indexDelete + 1; i <= size(); i++) {
+        for (int i = indexDelete + 1; i <= size; i++) {
             storage[i - 1] = storage[i];
             storage[i] = null;
         }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size());
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
