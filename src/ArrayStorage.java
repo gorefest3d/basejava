ed05f3bedf6167581ce3d27845d3bcc843eb3f73
Alex Sorokin <gorefest3d@mail.ru>
2021-06-05 14:48:10 +0300

    1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -35,7 +35,6 @@ public class ArrayStorage {
                     storage[j - 1] = storage[j];
                 }
                 size--;
-                
                 break;
             }
         }
