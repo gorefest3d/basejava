3dee59137f869f683dc4f4d9e68e7cbc65101ff8
Alex Sorokin <gorefest3d@mail.ru>
2021-06-30 02:05:45 +0300

    Lesson04 Reflection and JUnit

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -12,8 +12,12 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
 
+    protected AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         storage.clear();
         storage.save(new Resume(UUID_1));
         storage.save(new Resume(UUID_2));
@@ -42,11 +46,15 @@ public abstract class AbstractArrayStorageTest {
     }
 
     @Test(expected = NotExistStorageException.class)
-    public void getNotExist() throws Exception {
+    public void getNotExist() {
         storage.get("dummy");
     }
 
     @Test
     public void delete() {
+    }
+
+    @Test
+    public void get() {
     }
 }
--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -2,4 +2,7 @@ package com.urise.webapp.storage;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest{
 
+    protected ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
 }
