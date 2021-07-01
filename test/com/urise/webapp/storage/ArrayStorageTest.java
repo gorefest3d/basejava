3e0a2a6880716dead3deb06da16d017ec4bea938
Alex Sorokin <gorefest3d@mail.ru>
2021-07-02 02:58:44 +0300

    HW4

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -2,7 +2,7 @@ package com.urise.webapp.storage;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest{
 
-    protected ArrayStorageTest() {
+    public ArrayStorageTest() {
         super(new ArrayStorage());
     }
 }
