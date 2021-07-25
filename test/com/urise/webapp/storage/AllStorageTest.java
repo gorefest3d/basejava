2514ffc46f662f3e1e21974c0b6fbd69792f2667
Alex Sorokin <gorefest3d@mail.ru>
2021-07-25 18:43:41 +0300

    Lesson06 add AllTestStorage

---
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -0,0 +1,19 @@
+package com.urise.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                ArrayStorageTest.class,
+                SortedArrayStorageTest.class,
+                ListStorageTest.class,
+                MapByUuidStorageTest.class,
+                MapByResumeStorageTest.class
+
+        }
+)
+
+public class AllStorageTest {
+}
