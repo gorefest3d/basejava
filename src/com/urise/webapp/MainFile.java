f8efd9762d83b9d9b0f679d7f7e4350dd3b4e2ec
Alex Sorokin <gorefest3d@mail.ru>
2021-08-07 00:26:23 +0300

    Lesson08 add Position

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -5,7 +5,21 @@ import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) throws IOException {
-        File file = new File(".\\.gitignore");
-        System.out.println(file.getCanonicalPath());
+        File directory = new File(".\\src\\com\\urise\\webapp");
+
+        viewDirectoryDeep(directory);
+    }
+
+    public static void viewDirectoryDeep(File directory) {
+        File[] files = directory.listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println("File: " + file.getName());
+            } else if (file.isDirectory()) {
+                System.out.println("Directory: " + file.getName());
+                viewDirectoryDeep(file);
+            }
+        }
     }
 }
