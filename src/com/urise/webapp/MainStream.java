5a676d938bcf65347112f5a8a2b54b9dc76b1902
Alex Sorokin <gorefest3d@mail.ru>
2021-11-29 19:17:52 +0300

    Lesson12 HW

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -1,38 +1,31 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class MainStream {
-    public static String minValue(int[] values) {
-        return IntStream.of(values)
-                .distinct()
-                .sorted()
-                .mapToObj(Integer::toString).collect(Collectors.joining());
-    }
-
-    public static List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().mapToInt(numb -> numb).sum() % 2 != 0 ?
-                integers.stream().filter(i -> i % 2 != 0)
-                        .collect(Collectors.toList())
-                : integers.stream().filter(i -> i % 2 == 0)
-                .collect(Collectors.toList());
-    }
-
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 1, 6, 2, 2, 3, 7};
+        int[] values = {1, 2, 3, 5, 6, 2, 2, 3, 7};
 
-        List<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(8);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
+        List<Integer> list = Arrays.asList(4, 5, 2);
 
         System.out.println(minValue(values));
         System.out.println(oddOrEven(list));
+    }
+
+    public static int minValue(int[] values) {
+        return IntStream.of(values)
+                .distinct()
+                .sorted()
+                .reduce(0,(x, y) -> (x*10)+y);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(numb -> numb).sum();
+        return sum % 2 != 0 ?
+                integers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList())
+                : integers.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
     }
 }
